97264c571471a461778b5ed93b39bb3268436075
Ajgorek04 <igorbledzinski12@gmail.com>
2025-04-19 23:26:10 +0200

    pytania

---
+++ build/tests/CMakeFiles/test_question.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "tests/test_question_autogen/timestamp" "custom" "tests/test_question_autogen/deps"
+  "F:/Coding/ProjektQuiz/tests/test_question.cpp" "tests/CMakeFiles/test_question.dir/test_question.cpp.obj" "gcc" "tests/CMakeFiles/test_question.dir/test_question.cpp.obj.d"
+  "F:/Coding/ProjektQuiz/build/tests/test_question_autogen/mocs_compilation.cpp" "tests/CMakeFiles/test_question.dir/test_question_autogen/mocs_compilation.cpp.obj" "gcc" "tests/CMakeFiles/test_question.dir/test_question_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CMakeFiles/test_question.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_question.dir/test_question.cpp.obj"
+  "CMakeFiles/test_question.dir/test_question.cpp.obj.d"
+  "CMakeFiles/test_question.dir/test_question_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/test_question.dir/test_question_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/test_question_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/test_question_autogen.dir/ParseCache.txt"
+  "test_question_autogen"
+  "libtest_question.dll.a"
+  "test_question.exe"
+  "test_question.exe.manifest"
+  "test_question.pdb"
+  "test_question_autogen/mocs_compilation.cpp"
+  "test_question_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_question.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
